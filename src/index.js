54958a11f98c5a20a27d4bf2cb9f7929374384ba
Shang Qiao <shangqiao1996@163.com>
2022-09-17 11:26:09 +0800

    devServer and html-webpack-plugin

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import $ from "jquery";
 
 $(function () {
-  $("li:odd").css("background-color", "red");
+  $("li:odd").css("background-color", "green");
   $("li:even").css("background-color", "blue");
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,11 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
 module.exports = {
   mode: "development", // production
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: "./index.html", // 生成后放到项目根目录
+      template: "./src/index.html",
+    }),
+  ],
 };
